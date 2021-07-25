ca12ac03b004f4f3242aadebddd5cc177e80a87b
Raanjeetsgolu <76631820+Raanjeetsgolu@users.noreply.github.com>
2021-07-25 00:54:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var input=document.getElementById("input")
+function dis(val){
+   input.value+=val
+}
+
+function solve(){
+var y=eval(input.value)
+input.value=y
+}
+function clr(){
+input.value=""
+}
